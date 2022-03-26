8860493f8a01dea25109a1a7fad83ac7d18b0bea
Junaid Alam <102413407+Junaid2102@users.noreply.github.com>
2022-03-27 00:21:09 +0500

    Create main.py

---
+++ Breadth First Search with Python/main.py
@@ -0,0 +1,27 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F', 'G'],
+  'D' : [],
+  'E' : [],
+  'F' : [],
+  'G' : []
+}
+
+visited = []
+queue = []
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0)
+    print(s, end = " ")
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+print("Following is the Breadth First:")
+bfs(visited, graph, 'A')
